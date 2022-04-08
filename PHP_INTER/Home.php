4937a35dc05d6871a27b9f5a1e0e242a42526a0a
ChoChoTheint <79312349+ChoChoTheint@users.noreply.github.com>
2022-04-08 14:15:18 +0630

    Add files via upload

---
+++ PHP_INTER/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home{
+    public function __construct()
+    {
+        echo "I am constructor of " . __CLASS__ . " class <br/>";
+    }
+    public function index(){
+        echo "I am index method of " . __CLASS__ . "class<br/>";
+    }
+    public function show(){
+        echo "I am show method of " . __CLASS__ . "class<br/>";
+    }
+}
---
+++ PHP_INTER/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Post
+{
+    public function __construct()
+    {
+        echo "I am constructor of " . __CLASS__ . " class <br/>";
+    }
+
+    public function index()
+    {
+        echo "I am index method of " . __CLASS__ . "class<br/>";
+    }
+    public function show(){
+        echo "I am show method of " . __CLASS__ . "class<br/>";
+    }
+}
